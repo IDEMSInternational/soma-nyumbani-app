a44dd0b552b93512f5e3a4f97ad42368e93b183f
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-03 12:34:55 -0700

    Initial Commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { DbService } from 'src/app/services/db.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor(private db: DbService) {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/services/db.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as PouchDB from 'pouchdb/dist/pouchdb';
+// PouchDB.plugin(require('pouchdb-authentication'));
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DbService {
+  localDB = new PouchDB('somanyumbani');
+  remoteDB = new PouchDB('https://somanyumbani:somanyumbani@db.somanyumbani.com');
+  constructor() {
+    console.log('local', this.localDB);
+    console.log('remote', this.remoteDB);
+    this._syncRemoteDB();
+  }
+
+  private _syncRemoteDB() {
+    // this.localDB.logIn('somanyumbani', 'somanyumbani').then(() => {
+    //   console.log('logged into db');
+    // });
+    this.localDB.replicate
+      .from(this.remoteDB)
+      .on('complete', () => {
+        // yay, we're done!
+        console.log('replication complete');
+      })
+      .on('error', () => {
+        // boo, something went wrong!
+      });
+  }
+}
