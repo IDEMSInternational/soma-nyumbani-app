32a9fa0bcdccf0e2526967826927dd903c4f62b8
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-14 16:15:03 -0700

    add main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 
 import { Platform } from "@ionic/angular";
 import { Plugins } from "@capacitor/core";
+import { AnalyticsService } from "./services/analytics.service";
+import { UserService } from "./services/user.service";
+import { environment } from "src/environments/environment";
 const { SplashScreen } = Plugins;
 
 @Component({
@@ -10,11 +13,19 @@ const { SplashScreen } = Plugins;
   styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(private platform: Platform) {
+  APP_VERSION = environment.APP_VERSION;
+  constructor(
+    private platform: Platform,
+    private analyticsService: AnalyticsService,
+    private userService: UserService
+  ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {});
+    this.platform.ready().then(() => {
+      this.analyticsService.init();
+      this.userService.init();
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouteReuseStrategy } from "@angular/router";
-
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { FileOpener } from "@ionic-native/file-opener/ngx";
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { environment } from "src/environments/environment";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { FileOpener } from "@ionic-native/file-opener/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.FIREBASE_CONFIG),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule,
+  ],
   providers: [
     FileOpener,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
