85969c4edc5e90734237ababb2cc441c89dd7546
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-20 16:13:10 -0700

    update home page to show content by days

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { DbService } from "src/app/services/db.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-home",
@@ -6,5 +9,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./home.page.scss"],
 })
 export class HomePage {
-  constructor() {}
+  setActiveDay(activeDay: number) {
+    console.log("set active day", activeDay);
+    this.userService.updateUser({ activeDay });
+  }
+  constructor(
+    public db: DbService,
+    public userService: UserService,
+    private router: Router
+  ) {}
+
+  goToSession(session) {
+    this.router.navigate(["/report"]);
+  }
 }
