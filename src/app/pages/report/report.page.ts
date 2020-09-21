4fe7431a338bcc2871bf4dfe85e598ccf9619a0d
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-20 21:25:44 -0700

    report page pdf bindings and tidying

--- src/app/pages/report/report.page.ts
+++ src/app/pages/report/report.page.ts
@@ -1,19 +1,17 @@
 import {
   AfterViewInit,
+  ChangeDetectorRef,
   Component,
-  ElementRef,
-  OnInit,
+  OnDestroy,
   QueryList,
-  ViewChild,
   ViewChildren,
 } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ModalController } from "@ionic/angular";
-import { PdfViewerModalComponent } from "src/app/components/pdf-viewer-modal";
+import { Subscription } from "rxjs";
 import { SessionAttachmentComponent } from "src/app/components/session-attachment";
 import { SessionsListModalComponent } from "src/app/components/sessions-list-modal";
 import { DbService } from "src/app/services/db.service";
-import { FileService } from "src/app/services/file.service";
 import { UserService } from "src/app/services/user.service";
 import { DEFAULT_REPORT, ISessionReport } from "src/models";
 
@@ -26,53 +24,92 @@ import { DEFAULT_REPORT, ISessionReport } from "src/models";
  * Presents key information for user to generate a new session report, and start a session
  * TODO - inputs might be better formatted within a form with general change bindings
  */
-export class ReportPage implements AfterViewInit {
+export class ReportPage implements AfterViewInit, OnDestroy {
   // Provide a dynamic binding for session attachment components so that the download
   // status can be confirmed before proceeding to a session
   @ViewChildren("attachment") attachments: QueryList<
     SessionAttachmentComponent
   >;
-  report: ISessionReport = DEFAULT_REPORT;
+  sessionGuideStatus: "downloaded" | "downloading";
+  report: ISessionReport;
+  attachments$: Subscription;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     public db: DbService,
     private modalCtrl: ModalController,
-    private user: UserService,
-    private fileService: FileService
+    private userService: UserService,
+    private cdr: ChangeDetectorRef
   ) {
-    const { reportId } = route.snapshot.queryParams;
-    if (reportId) {
-      console.log("report id found, loading report", reportId);
-      // TODO
+    this.init();
+  }
+  /**
+   * During initialisation identify the day and session number the report refers to using
+   * route query params, load any existing report that exists, otherwise create with
+   * default template
+   */
+  private init() {
+    const {
+      session_number,
+      day_number,
+      selectedSession,
+    } = this.route.snapshot.queryParams;
+    if (session_number && day_number && selectedSession) {
+      const _id = `day-${day_number}_session-${session_number}`;
+      if (this.userService.user.sessionReports[_id]) {
+        this.report = this.userService.user.sessionReports[_id];
+        console.log("loading existing report", this.report);
+      } else {
+        console.log("creating new report");
+        this.report = {
+          ...DEFAULT_REPORT,
+          _id,
+          selectedSession,
+          day_number,
+          session_number,
+        };
+      }
+    } else {
+      // no session selected, return to home page
+      this.router.navigate(["/"]);
     }
-    if (!reportId) {
-      console.log("no report id, creating");
-      this.router.navigate([], {
-        relativeTo: this.route,
-        queryParams: { reportId: this.report._id },
-        queryParamsHandling: "merge", // remove to replace all query params by provided
-      });
-    }
-    //
   }
   ngAfterViewInit() {
-    // TODO - use value to confirm if all attachments downloaded, and add option to button
-    // TODO - add similar logic to display simple master download button in sessions main list
-    this.attachments.changes.subscribe((c) => {
-      console.log("attachments", c);
-    });
+    this._updateAttachmentDownloadStatus();
+    this._subscribeToAttachmentChanges();
+  }
+  /**
+   * The report doesn't natively know if attachments have been downloaded, so observe for
+   * changes to child session-attachment components to inform
+   */
+  private _subscribeToAttachmentChanges() {
+    this.attachments$ = this.attachments.changes.subscribe(() =>
+      this._updateAttachmentDownloadStatus()
+    );
+  }
+  /**
+   * Simple check if session-attachment components exist, and if the first component has
+   * been marked as downloaded
+   */
+  private _updateAttachmentDownloadStatus() {
+    const { first } = this.attachments;
+    if (first && first.isDownloaded) {
+      this.sessionGuideStatus = "downloaded";
+      this.cdr.detectChanges();
+    }
+  }
+  ngOnDestroy() {
+    this.attachments$.unsubscribe();
   }
 
   updateReport(key: keyof ISessionReport, value: any) {
     console.log("update report", key, value);
     this.report[key as any] = value;
     const { _id } = this.report;
-    this.user.updateUserDoc("sessionReports", _id, this.report, false);
+    this.userService.updateUserDoc("sessionReports", _id, this.report, false);
   }
 
   async openSessionSelect() {
-    console.log("opening session select");
     const modal = await this.modalCtrl.create({
       component: SessionsListModalComponent,
       componentProps: { selectedSession: this.report.selectedSession },
@@ -81,14 +118,20 @@ export class ReportPage implements AfterViewInit {
     const { data } = await modal.onDidDismiss();
     this.updateReport("selectedSession", data);
   }
-  async goToSession(attachment) {
-    console.log("attachment", attachment);
-    console.log("attachments", this.attachments);
-    this.attachments.forEach((a) => {
-      console.log("a", a, a.isDownloaded);
-    });
-    // assume if opening criteria met to no longer count as draft
-    // this.updateReport("_draft", false);
-    // this.fileService.openAttachment();
+  /**
+   * Again, the report doesn't have native functionality to open the session guide pdf
+   * so call on first session-attachment component (assuming it is the guide)
+   * and default open method
+   */
+  async goToSession() {
+    // assume the report is now not draft, so save
+    this.updateReport("_draft", false);
+    this.attachments.first.handleAttachmentClick();
+  }
+  async downloadSessionGuide() {
+    this.sessionGuideStatus = "downloading";
+    await this.attachments.first.downloadAttachment();
+    this.sessionGuideStatus = "downloaded";
+    this.cdr.detectChanges();
   }
 }
